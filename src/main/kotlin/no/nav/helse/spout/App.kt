66bcc1e02d109e711815da4d425f534911b0a3c6
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-03-23 14:57:49 +0100

    Bygg da 🤷‍

--- src/main/kotlin/no/nav/helse/spout/App.kt
+++ src/main/kotlin/no/nav/helse/spout/App.kt
@@ -1,16 +1,24 @@
 package no.nav.helse.spout
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.slf4j.LoggerFactory
+
+private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
 
 fun main() {
     embeddedServer(CIO, port = 8080) {
         routing {
             get("/isalive") { call.respondText("ALIVE!") }
             get("/isready") { call.respondText("READY!") }
+            post("/melding") {
+                sikkerlogg.info("Sender melding")
+                call.respond(HttpStatusCode.Accepted)
+            }
         }
     }.start(wait = true)
 }
