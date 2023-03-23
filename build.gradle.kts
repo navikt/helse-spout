af1ac3d904d5a9dfe839ba9185c6e76769db9ce7
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-03-23 14:11:50 +0100

    Bygger ei lita jar 🤏

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,4 +30,23 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = javaVersion
     }
+
+    withType<Jar> {
+        archiveBaseName.set("app")
+
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.spout.AppKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
+        }
+    }
 }
