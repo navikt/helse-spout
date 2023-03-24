2902a825c290f5855221a54ee71d46d1d1e60a28
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-03-24 07:46:01 +0100

    Ørliten try-catch 🪂

--- src/main/kotlin/no/nav/helse/spout/Routes.kt
+++ src/main/kotlin/no/nav/helse/spout/Routes.kt
@@ -8,6 +8,7 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.slf4j.LoggerFactory
+import java.lang.Exception
 import java.time.LocalDateTime
 
 private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
@@ -29,30 +30,36 @@ internal fun Route.spout(
     }
 
     post("/melding") {
-        val avsender = jacksonObjectMapper().createObjectNode()
-            .put("NAVIdent", navIdent(call))
-            .put("navn", navn(call))
-            .put("epost", epost(call))
+        val (metadata, melding) = try {
+            val avsender = jacksonObjectMapper().createObjectNode()
+                .put("NAVIdent", navIdent(call))
+                .put("navn", navn(call))
+                .put("epost", epost(call))
 
-        val parameters = call.receiveParameters()
-        val fødselsnummer = parameters.hent("fodselsnummer")
-        val tidspunkt = LocalDateTime.now()
+            val parameters = call.receiveParameters()
+            val fødselsnummer = parameters.hent("fodselsnummer")
+            val tidspunkt = LocalDateTime.now()
 
-        val json = objectMapper.readTree(Template.resolve(
-            input = parameters.hent("json"),
-            navIdent = navIdent(call),
-            navn = navn(call),
-            epost = epost(call),
-            tidspunkt = tidspunkt
-        )) as ObjectNode
+            val json = objectMapper.readTree(Template.resolve(
+                input = parameters.hent("json"),
+                navIdent = navIdent(call),
+                navn = navn(call),
+                epost = epost(call),
+                tidspunkt = tidspunkt
+            )) as ObjectNode
 
-        json.put("@event_name", parameters.hent("event_name"))
-        json.put("fødselsnummer", fødselsnummer)
-        json.put("aktørId", parameters.hent("aktorId"))
-        json.replace("@avsender", avsender)
+            json.put("@event_name", parameters.hent("event_name"))
+            json.put("fødselsnummer", fødselsnummer)
+            json.put("aktørId", parameters.hent("aktorId"))
+            json.replace("@avsender", avsender)
 
-        val (metadata, melding) = sender.send(fødselsnummer, json, tidspunkt)
-        sikkerlogg.info("Sendt melding fra Spout\nMelding:\n\t: $melding\nMetadata:\n\t $metadata")
+            val (metadata, melding) = sender.send(fødselsnummer, json, tidspunkt)
+            sikkerlogg.info("Sendt melding fra Spout\nMelding:\n\t: $melding\nMetadata:\n\t $metadata")
+            metadata to melding
+        } catch (ex: Exception) {
+            sikkerlogg.error("Feil ved sending av melding", ex)
+            objectMapper.createObjectNode().put("feil", "${ex.message}") to objectMapper.createObjectNode()
+        }
 
         val html = KVITTERING
             .replace("{{json}}", melding.toPrettyString())
