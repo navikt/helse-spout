eb4cc273fdc57dff07c71b07db20217cb6ff62ee
david steinsland <david@davidsteinsland.net>
2025-03-27 20:15:59 +0100

    bumper avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-val ktorVersion = "3.0.1"
+val ktorVersion = "3.1.2"
 val logbackClassicVersion = "1.5.12"
 val logbackEncoderVersion = "8.0"
-val jacksonVersion = "2.18.1"
+val jacksonVersion = "2.18.3"
 val kafkaVersion = "3.9.0"
-val junitJupiterVersion = "5.11.3"
+val junitJupiterVersion = "5.12.1"
 
 plugins {
     kotlin("jvm") version "2.1.20"
