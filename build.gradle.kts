e2154896028b45da8563761a3135e2dfacf82a93
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-16 10:18:06 +0200

    Bumper dependencies
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 val ktorVersion = "3.2.3"
-val logbackClassicVersion = "1.5.12"
+val logbackClassicVersion = "1.5.18"
 val logbackEncoderVersion = "8.0"
 val jacksonVersion = "2.18.3"
-val kafkaVersion = "3.9.0"
+val kafkaVersion = "4.1.0"
 val junitJupiterVersion = "5.12.1"
 
 plugins {
